2fd1c9515827840549be448dad7a94ffd9619b69
onepro19 <w.settiawan@gmail.com>
2020-12-29 18:50:56 +0700

    last fixed

--- sw.js
+++ sw.js
@@ -1,89 +1,46 @@
-const CACHE_NAME = "one-soccer-v1.08";
-const urlsToCache = [
-    "/",
-    "/components/nav.html",
-    "/css/materialize.min.css",
-    "/css/style.css",
-    "/img/Fifa.svg",
-    "/img/logo.png",
-    "/js/api.js",
-    "/js/competitions.js",
-    "/js/db.js",
-    "/js/idb.js",
-    "/js/main.js",
-    "/js/matches.js",
-    "/js/materialize.min.js",
-    "/js/nav.js",
-    "/pages/competitions.html",
-    "/pages/detail-competitions.html",
-    "/pages/last-matches.html",
-    "/pages/matches.html",
-    "/pages/saved.html",
-    "/.eslintrc.json",
-    "/index.html",
-    "/manifest.json",
-    "/sw.js"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+    
+if (workbox)
+    console.log(`Workbox berhasil dimuat`);
+else
+    console.log(`Workbox gagal dimuat`);
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
+workbox.precaching.precacheAndRoute([
+    {url:'/', revision:'1'},
+    {url:'/components/nav.html', revision:'1'},
+    {url:'/css/materialize.min.css', revision:'1'},
+    {url:'/css/style.css', revision:'1'},
+    {url:'/img/Fifa.svg', revision:'1'},
+    {url:'/img/logo.png', revision:'1'},
+    {url:'/js/api.js', revision:'1'},
+    {url:'/js/competitions.js', revision:'1'},
+    {url:'/js/db.js', revision:'1'},
+    {url:'/js/idb.js', revision:'1'},
+    {url:'/js/main.js', revision:'1'},
+    {url:'/js/matches.js', revision:'1'},
+    {url:'/materialize.min.js', revision:'1'},
+    {url:'/js/nav.js', revision:'1'},
+    {url:'/pages/competitions/html', revision:'1'},
+    {url:'/pages/detail-competitions.html', revision:'1'},
+    {url:'/pages/last-matches.html', revision:'1'},
+    {url:'/pages/matches.html', revision:'1'},
+    {url:'/pages/saved.html', revision:'1'},
+    {url:'/.eslintrc.json', revision:'1'},
+    {url:'/manifest.json', revision:'1'},
+    {url:'/push.js', revision:'1'},
+    {url:'/sw.js', revision:'1'}
+]);
 
-self.addEventListener("fetch", event => {
-    const base_url = "https://api.football-data.org/v2/";
-    if (event.request.url.indexOf(base_url) > -1) {
-        event.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-            return fetch(event.request).then( response => {
-                cache.put(event.request.url, response.clone());
-                return response;
-            });
-        })
-    );
-    } else {
-        event.respondWith(
-        caches.match(event.request, {ignoreSearch: true}).then((response) => {
-            return response || fetch (event.request);
-            })
-        );
-    }
-});
+workbox.routing.registerRoute(
+    new RegExp('/pages/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'pages'
+    })
+);
 
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if(cacheName != CACHE_NAME) return caches.delete(cacheName);
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener('push', function(event) {
-    var body;
-    if (event.data) {
-        body = event.data.text();
-    } else {
-        body = 'Push message no payload';
-    }
-
-    var options = {
-        body: body,
-        icon: 'img/notification.png',
-        vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: 1
-        }
-    };
-
-    event.waitUntil(
-        self.registration.showNotification('Push Notification', options)
-    );
-});
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'google-fonts-stylesheets',
+    })
+);
